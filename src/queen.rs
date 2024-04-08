b4f7a82e9bf608f41b1fbafcd15feb36fd5ebe47
Stacy <s.shonn.o16@gmail.com>
2024-04-08 16:21:39 -0400

    Added unit tests to king, rook, knight, and bishop

--- src/queen.rs
+++ src/queen.rs
@@ -3,12 +3,28 @@ use crate::bitboard::Bitboard;
 pub const QUEENS_START: Bitboard = Bitboard::new(0b0001000000000000000000000000000000000000000000000000000000010000);
 
 pub fn queen_all_moves(queens: Bitboard, obstiacles: Bitboard) -> Bitboard{
-   queens.fill(obstiacles, Bitboard::north_one) |
+   (queens.fill(obstiacles, Bitboard::north_one) |
    queens.fill(obstiacles, Bitboard::north_east_one) |
    queens.fill(obstiacles, Bitboard::north_west_one) |
    queens.fill(obstiacles, Bitboard::south_one) |
    queens.fill(obstiacles, Bitboard::south_east_one) |
    queens.fill(obstiacles, Bitboard::south_west_one) |
    queens.fill(obstiacles, Bitboard::east_one) |
-   queens.fill(obstiacles, Bitboard::west_one)
+   queens.fill(obstiacles, Bitboard::west_one)) ^ queens
+}
+
+#[cfg(test)]
+mod tests{
+
+    #[test]
+    fn queen_all_moves(){
+        let mut bishops = crate::bitboard::Bitboard::new(0);
+        bishops.flip_bit(36);
+        let mut obsticales = crate::bitboard::Bitboard::new(0);
+        let mut moves = super::queen_all_moves(bishops, obsticales);
+        assert_eq!(moves.popultion_count(), 27);
+        obsticales.flip_bit(35);
+        moves = super::queen_all_moves(bishops, obsticales);
+        assert_eq!(moves.popultion_count(), 24);
+    }
 }
--- src/rook.rs
+++ src/rook.rs
@@ -3,8 +3,24 @@ use crate::bitboard::Bitboard;
 pub const ROOKS_START: Bitboard = Bitboard::new(0b1000000100000000000000000000000000000000000000000000000010000001);
 
 pub fn rook_all_moves(rooks: Bitboard, obsticales: Bitboard) -> Bitboard{
-    rooks.fill(obsticales, Bitboard::north_one)| 
+    (rooks.fill(obsticales, Bitboard::north_one)| 
     rooks.fill(obsticales, Bitboard::south_one)| 
     rooks.fill(obsticales, Bitboard::east_one)| 
-    rooks.fill(obsticales, Bitboard::west_one)
+    rooks.fill(obsticales, Bitboard::west_one)) ^ rooks
+}
+
+#[cfg(test)]
+mod tests{
+
+    #[test]
+    fn rook_all_moves(){
+        let mut rook = crate::bitboard::Bitboard::new(0);
+        rook.flip_bit(0);
+        let mut obsticales = crate::bitboard::Bitboard::new(0);
+        let mut moves = super::rook_all_moves(rook, obsticales);
+        assert_eq!(moves.popultion_count(), 14);
+        obsticales.flip_bit(1);
+        moves = super::rook_all_moves(rook, obsticales);
+        assert_eq!(moves.popultion_count(), 8);
+    }
 }
